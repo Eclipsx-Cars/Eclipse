11641388f6d3ad084c13bc3d864d268802745b72
qrakendev <admin@eclipsx.co.uk>
2024-10-25 16:46:59 +0100

    New Styles

--- rentacar-main/src/components/landingPage.tsx
+++ rentacar-main/src/components/landingPage.tsx
@@ -1,28 +1,44 @@
 import React from 'react';
-import About from './about';
-import Cars from './Cars';
-import ReviewCarousel from './review';
-import Header from "./header";
-
+import { Link } from 'react-router-dom'; // Import Link for navigation
+import '../css/Button.css'
 
 const LandingPage = () => {
     return (
-        <div>
-            <Header />
-            <div className="bg-cover bg-center h-screen grid place-items-center"
-                style={{ backgroundImage: `url(${require('../assets/BackgroundLogoBest.jpg')})` }}>
-                <div className="relative min-h-screen flex items-end justify-center">
+        <div className="overflow-hidden">
+            {/*<Header />*/}
+            {/* Set the container to take exactly the height of the viewport */}
+            <div className="bg-cover bg-center min-h-screen w-full flex flex-col justify-center items-center"
+                 style={{ backgroundImage: `url(${require('../assets/BackgroundLogoBest.jpg')})` }}>
+
+                {/* Heading "Luxury Car Rentals" */}
+                <div className="mt-20 sm:mt-80 text-center px-4 w-full">
                     <h1
-                        className="absolute text-center text-6xl text-white whitespace-nowrap"
-                        style={{ textShadow: '1px 1px 0 #000', bottom: '10rem' }}
+                        className="text-4xl sm:text-6xl text-white whitespace-nowrap"
+                        style={{ textShadow: '1px 1px 0 #000', marginBottom: '1rem' }}
                     >
                         Luxury Car Rentals
                     </h1>
+
+                    {/* Buttons aligned below the heading */}
+                    <div className="flex flex-wrap justify-center space-x-2 sm:space-x-4">
+                        <Link to="/About">
+                            <button className="luxury-button">About</button>
+                        </Link>
+                        <Link to="/reviews">
+                            <button className="luxury-button">Reviews</button>
+                        </Link>
+                        <Link to="/Cars">
+                            <button className="luxury-button">Cars</button>
+                        </Link>
+                        <Link to="/Signin">
+                            <button className="luxury-button">Sign in</button>
+                        </Link>
+                        <Link to="/Register">
+                            <button className="luxury-button">Register</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
-            <About />
-            <ReviewCarousel />
-            <Cars />
         </div>
     );
 };
--- rentacar-main/src/components/review.tsx
+++ rentacar-main/src/components/review.tsx
@@ -3,6 +3,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import axios from 'axios';
+import '../css/Review.css'
+import Header from "./header";
 
 interface Review {
   id?: string;
@@ -53,59 +55,62 @@ const ReviewCarousel: React.FC = () => {
   };
 
   return (
-      <div className='bg-gray-100'>
-        <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Reviews</h2>
-        <div className="max-w-2xl mx-auto p-4 flex flex-col items-center justify-center min-h-screen">
-          <Slider {...settings} className="w-full">
-            {reviews.map((review, index) => (
-                <div key={index} className="p-4">
-                  <div className="bg-white rounded-lg shadow-lg p-6 text-center">
-                    <p className="text-lg font-semibold mb-2">{review.name}</p>
-                    <p className="text-gray-700">{review.content}</p>
+      <div>
+        <Header />
+        <div className='bg-gray-100 mt-20'>
+          <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Reviews</h2>
+          <div className="max-w-2xl mx-auto p-4 flex flex-col items-center justify-center min-h-screen">
+            <Slider {...settings} className="w-full">
+              {reviews.map((review, index) => (
+                  <div key={index} className="p-4">
+                    <div className="bg-white rounded-lg shadow-lg p-6 text-center">
+                      <p className="text-lg font-semibold mb-2">{review.name}</p>
+                      <p className="text-gray-700">{review.content}</p>
+                    </div>
                   </div>
-                </div>
-            ))}
-          </Slider>
+              ))}
+            </Slider>
 
-          <div className="mt-8 w-full">
-            <h2 className="text-2xl font-semibold mb-4 text-center">Add a Review</h2>
-            <form onSubmit={handleAddReview} className="bg-white rounded-lg shadow-lg p-6">
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
-                  Name
-                </label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    value={newReview.name}
-                    onChange={handleInputChange}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    required
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">
-                  Review
-                </label>
-                <textarea
-                    name="content"
-                    id="content"
-                    value={newReview.content}
-                    onChange={handleInputChange}
-                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                    required
-                ></textarea>
-              </div>
-              <div className="flex items-center justify-between">
-                <button
-                    type="submit"
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                >
-                  Add Review
-                </button>
-              </div>
-            </form>
+            <div className="mt-8 w-full">
+              <h2 className="text-2xl font-semibold mb-4 text-center">Add a Review</h2>
+              <form onSubmit={handleAddReview} className="bg-white rounded-lg shadow-lg p-6">
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+                    Name
+                  </label>
+                  <input
+                      type="text"
+                      name="name"
+                      id="name"
+                      value={newReview.name}
+                      onChange={handleInputChange}
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      required
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="content">
+                    Review
+                  </label>
+                  <textarea
+                      name="content"
+                      id="content"
+                      value={newReview.content}
+                      onChange={handleInputChange}
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      required
+                  ></textarea>
+                </div>
+                <div className="flex items-center justify-between">
+                  <button
+                      type="submit"
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                  >
+                    Add Review
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
