5d6f10d84ac18ab1ecae864d139f88604eb30209
Qaiser Hussain <quickstopautos@outlook.com>
2025-06-18 23:34:05 +0100

    Updated gitignore to exclude node_modules and other unnecessary files

---
+++ rentacar-main/src/types/car.ts
@@ -0,0 +1,11 @@
+export interface Car {
+    id: string;
+    make: string;
+    model: string;
+    year: number;
+    description: string;
+    price: string;
+    CarForReason: string;
+    imageUrl?: string;
+    images: string[];
+}
